47d8e54c0235636e61fa3577c05c66dca06a1fca
Tommy <54972963+EWCMF@users.noreply.github.com>
2020-09-16 00:56:10 +0200

    added test picture and changed server code

--- webserver.js
+++ webserver.js
@@ -1,21 +1,34 @@
 var http = require('http');
-var url = require('url');
 var fs = require('fs');
+var path = require('path')
 
 http.createServer(function (req, res) {
-  let q = url.parse(req.url, true);
-  let filename;
-  if (q.pathname == "/") {
-    filename = "./index.html";
-  } else {
-    filename = "." + q.pathname;
+  let filePath = req.url;
+  if (filePath == "/") {
+    filePath = "/index.html";
   }
-  fs.readFile(filename, function(err, data) {
+
+  filePath = __dirname + filePath;
+  let contentType;
+  
+  let ext = path.extname(filePath);
+  switch (ext) {
+    case ".html":
+      contentType = 'text/html';
+      break;
+    case ".css":
+      contentType = 'text/css'  
+      break;
+    case ".png":
+      contentType = 'image/png'  
+  }
+
+  fs.readFile(filePath, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    }
+    res.writeHead(200, {'Content-Type': contentType});
     res.write(data);
     return res.end();
   });
